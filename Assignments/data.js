5824cb815cb25aaeebfdd4a226d9dd634998a997
venkatanaresh6 <vn525606@gmail.com>
2024-06-12 09:51:28 +0530

    task1

---
+++ Assignments/data.js
@@ -0,0 +1,28 @@
+function changecolor(){
+    let btn_ref=document.getElementsByTagName('button')[0]
+    btn_ref.style.backgroundColor='blue'
+
+}
+function changecolor1(){
+    let btn_ref1=document.getElementsByTagName('button')[1]
+    btn_ref1.style.backgroundColor='blue'
+
+}
+function changecolor2(){
+    let btn_ref1=document.getElementsByTagName('button')[2]
+    btn_ref1.style.backgroundColor='blue'
+
+}
+function changecolor3(){
+    let btn_ref1=document.getElementsByTagName('button')[3]
+    btn_ref1.style.backgroundColor='blue'
+
+}
+function changecolor4(){
+    let btn_ref1=document.getElementsByTagName('input')[0]
+    btn_ref1.style.backgroundColor='blue'
+}
+function changecolor5(){
+    let btn_ref1=document.getElementsByTagName('input')[1]
+    btn_ref1.style.backgroundColor='blue'
+}

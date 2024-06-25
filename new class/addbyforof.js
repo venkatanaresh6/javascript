36911adab9e7f7af91e9db717463c98a9aa864b2
venkatanaresh6 <vn525606@gmail.com>
2024-06-25 10:08:10 +0530

    .

--- new class/addbyforof.js
+++ new class/addbyforof.js
@@ -1,5 +1,6 @@
 let prices=[98,198,298,398]
-let new_prices=prices.map((price)=>{
-    return price+1
-})
+let new_prices=[]
+for(price of prices){
+    new_prices.push(price+1)
+}
 console.log(new_prices)
--- new class/addbymap.js
+++ new class/addbymap.js
@@ -1,6 +1,5 @@
 let prices=[98,198,298,398]
-let new_prices=[]
-for(price of prices){
-    new_prices.push(price+1)
-}
+let new_prices=prices.map((price)=>{
+    return price+1
+})
 console.log(new_prices)

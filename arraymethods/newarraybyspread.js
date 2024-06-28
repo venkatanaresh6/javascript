4bb0c36305aa8f2714afe04692a6b52b733e305c
venkatanaresh6 <vn525606@gmail.com>
2024-06-28 11:01:51 +0530

    spread and rest

---
+++ arraymethods/newarraybyspread.js
@@ -0,0 +1,6 @@
+eids=[101,102,103,104]
+new_eids=[...eids]
+console.log(new_eids)
+//we can extend using spread operator="..."
+new_eids1=[...new_eids,105,106]
+console.log(new_eids1)
---
+++ arraymethods/spreadasrest.js
@@ -0,0 +1,4 @@
+//"..." operator also used as rest
+//REST means ex:-[a,b] =[1,2,3,4,5]->a=1 b=2 to store rest 3,4,5 we use "...".
+let [a,...b]=[1,2,3,4,5,6,7,8,9]
+console.log(a,b)
